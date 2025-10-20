8845bb25eae0a899362ba249d98925634ebd7346
GameMoR <159117880+GameMoR1@users.noreply.github.com>
2025-10-20 10:54:18 +0400

    Update config.py

--- core/config.py
+++ core/config.py
@@ -8,4 +8,4 @@ WEBHOOK_ENABLED = True
 
 WEBHOOK_URL = 'https://n8n.kyter.space/webhook/a141441b-57cf-4c44-b131-bd86cb9c3a3a'
 
-LOGGER_API_URL = 'http://209.38.252.113:7998/log'
+LOGGER_API_URL = 'https://whisper.tuna-edu.ru/log'
